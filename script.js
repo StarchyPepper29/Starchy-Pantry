ea4073c0fa6f70abdc656bf5135190aa1c0e0075
Umais Usmani <98653230+StarchyPepper29@users.noreply.github.com>
2023-08-04 21:10:49 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM loaded! 🚀");
+  
+    const recipeList = document.getElementById("recipeList");
+  
+    fetch("./recipe-list.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (!Array.isArray(data)) {
+          throw new Error("JSON data is not an array");
+        }
+        data.forEach((recipe) => {
+          const recipeItem = document.createElement("div");
+          
+          recipeItem.classList.add("recipe-item");
+
+          const linkwrapper = document.createElement("a");
+          linkwrapper.href = "single.html?id=" + recipe.id;
+          const recipeName = document.createElement("h2");
+          recipeName.textContent = recipe.name;
+          recipeName.classList.add("nameclass");
+          const Expand = document.createElement("button");
+            Expand.textContent = "Expand";
+            Expand.classList.add("expandbutton");
+            
+          
+            
+          const recipeRegion = document.createElement("p");
+          recipeRegion.textContent = "Region: " + recipe.Region;
+          recipeRegion.classList.add("regionclass");
+          recipeRegion.classList.add("hidden");
+  
+          const recipeIngredients = document.createElement("ul");
+          
+          const recipeDescription = document.createElement("p");
+          recipeDescription.textContent = recipe.description;
+          recipeDescription.classList.add("descriptionclass");
+  
+          const recipeImage = document.createElement("img");
+          recipeImage.src = recipe.Image;
+          recipeImage.alt = recipe.Name;
+            recipeImage.classList.add("imageclass");
+
+          const ingredientList = document.createElement("ul"); // Create unordered list
+          ingredientList.classList.add("ingredientsclass"); // Add class to unordered list
+          ingredientList.classList.add("hidden"); // Hide unordered list
+          // Loop through the ingredient list array and create list items
+          recipe.ingredientList.forEach((ingredient) => {
+            const ingredientItem = document.createElement("li");
+            ingredientItem.textContent = ingredient;
+            ingredientList.appendChild(ingredientItem);
+          });
+  
+          const recipeShortInstructions = document.createElement("p");
+          recipeShortInstructions.textContent = recipe.shortInstructions;
+            recipeShortInstructions.classList.add("instructionsclass");
+            recipeShortInstructions.classList.add("hidden");
+
+          
+          linkwrapper.appendChild(recipeImage);
+          linkwrapper.appendChild(recipeName);
+          recipeItem.appendChild(Expand);
+          recipeItem.appendChild(recipeRegion);
+          
+          
+          recipeList.appendChild(linkwrapper);
+          recipeList.appendChild(recipeItem);
+          recipeList.appendChild(recipeShortInstructions);
+          
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  
+      
+  });
+  
---
+++ single.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const URLp= new URLSearchParams(window.location.search);
+    const id = URLp.get("id");
+    console.log(id);
+
+    fetch("./recipe-list.json")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            const recipe = data.find((item) => item.id === id);
+            if (!recipe) {
+              throw new Error("Recipe not found");
+            }
+            document.getElementById("recipeName").textContent = recipe.name;
+            document.getElementById("recipeRegion").textContent = "Region: " + recipe.region;
+      
+            const ingredientList = document.getElementById("recipeIngredients");
+            recipe.ingredientList.forEach((ingredient) => {
+              const listItem = document.createElement("li");
+              listItem.textContent = ingredient;
+              ingredientList.appendChild(listItem);
+            });
+      
+            document.getElementById("recipeDescription").textContent = recipe.description;
+            document.getElementById("recipeImage").src = recipe.image;
+            document.getElementById("recipeImage").alt = recipe.name;
+            document.getElementById("recipeShortInstructions").textContent = recipe.shortInstructions;
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+          });  
+});
+
